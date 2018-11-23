fb5c564248a8a5a322f8ae241eb163d415f27fe5
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-23 11:28:29 +0100

    Add a React functional component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a React component
-
+const App = () => {
+  return <div>Hi there!</div>;
+}
 
 // Take the React component and show it on the screen
