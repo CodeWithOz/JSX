20559f10285755051a6674a6076edff6b1553a90
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-24 07:15:53 +0100

    Make the 'for' attribute React compliant

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 // Create a React component
 const App = () => {
   const buttonText = { text: 'Click Me!' };
+  const labelText = 'Enter name:';
 
   return (
     <div>
-      <label className="label" for="name">Enter name:</label>
+      <label className="label" htmlFor="name">{labelText}</label>
       <input id="name" type="text"/>
       <button style={{
         backgroundColor: 'blue',
